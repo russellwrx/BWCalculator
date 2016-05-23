f527e4737cb6737d9481d2d3ac8f1ad817437103
russell <russell.gordeyev@gmail.com>
2016-05-23 17:24:15 +1000

    Release V1.01 Dark Theme

---
+++ app/src/main/java/com/demo/russell/bwsecurity/SummaryItem.java
@@ -0,0 +1,29 @@
+package com.demo.russell.bwsecurity;
+
+/**
+ * Created by russell on 21/05/16.
+ */
+public class SummaryItem {
+    private String CValue;
+    private String cName;
+
+
+
+    public String getcName() {
+        return cName;
+    }
+    public String getCValue() {
+        return CValue;
+    }
+
+
+    public void setCValue(String CValue) {
+        this.CValue = CValue;
+    }
+    public void setcName(String cName) {
+        this.cName = cName;
+    }
+
+
+
+}
