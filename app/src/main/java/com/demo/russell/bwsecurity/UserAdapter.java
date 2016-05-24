b0d59f01fe3deb72056cb845e353b8be8c3e2c43
russell <russell.gordeyev@gmail.com>
2016-05-24 10:56:55 +1000

    Release V1.02 Fixed amount single line

--- app/src/main/java/com/demo/russell/bwsecurity/UserAdapter.java
+++ app/src/main/java/com/demo/russell/bwsecurity/UserAdapter.java
@@ -1,6 +1,7 @@
 package com.demo.russell.bwsecurity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,6 +32,12 @@ public class UserAdapter extends ArrayAdapter<SummaryItem> {
         // Populate the data into the template view using the data object
         tvName.setText(user.cName);
         tvHome.setText(user.CValue);
+
+        if(user.cName=="--------"){
+            tvHome.setTextColor(Color.DKGRAY);
+        } else
+            tvHome.setTextColor(Color.WHITE);
+        //tvHome.setTextColor(Color.RED);
         // Return the completed view to render on screen
         return convertView;
     }
