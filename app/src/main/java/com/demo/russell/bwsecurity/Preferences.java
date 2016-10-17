3ef60bb95a3df4c736d615a731fcdd1062903316
russell <russell.gordeyev@gmail.com>
2016-10-17 13:57:39 +1100

    Settings Page Float adjustment Release 1

---
+++ app/src/main/java/com/demo/russell/bwsecurity/Preferences.java
@@ -0,0 +1,44 @@
+package com.demo.russell.bwsecurity;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+/**
+ * Created by russell on 16/10/2016.
+ */
+
+public class Preferences  extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.Preference);
+        setContentView(R.layout.activity_no_detail);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        MyPreferenceFragment myPreferenceFragment = new MyPreferenceFragment();
+        fragmentTransaction.add(android.R.id.content, myPreferenceFragment, "SETTINGS_FRAGMENT");
+        fragmentTransaction.commit();
+
+        }
+
+
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_screen);
+        }
+    }
+}
