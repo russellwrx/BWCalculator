3a6ef82df6f5ebfc2c04a8a461233bad6427d86b
russell <russell.gordeyev@gmail.com>
2016-10-21 19:19:29 +1100

    Settings Page Show Preference values on preference screen on the right Release 1.101

--- app/src/main/java/com/demo/russell/bwsecurity/EditTextPreference.java
+++ app/src/main/java/com/demo/russell/bwsecurity/EditTextPreference.java
@@ -2,23 +2,42 @@ package com.demo.russell.bwsecurity;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
 
 public class EditTextPreference extends android.preference.EditTextPreference {
+    private TextView textValue;
 
-    public EditTextPreference(Context context, AttributeSet attrs) {
+    public EditTextPreference(Context context){
+        super(context);
+        setLayoutResource(R.layout.preference_with_value);
+    }
+
+
+    public EditTextPreference(Context context, AttributeSet attrs){
         super(context, attrs);
+        setLayoutResource(R.layout.preference_with_value);
+    }
+
+    public EditTextPreference(Context context, AttributeSet attrs, int defStyle){
+        super(context,attrs,defStyle);
+        setLayoutResource(R.layout.preference_with_value);
     }
 
     @Override
-    protected void onDialogClosed(boolean positiveResult) {
-        super.onDialogClosed(positiveResult);
-
-        setSummary(getSummary());
+    protected void onBindView(View view){
+        super.onBindView(view);
+        textValue = (TextView) view.findViewById(R.id.preference_value);
+        if (textValue!=null)
+            textValue.setText(getText());
     }
 
     @Override
-    public CharSequence getSummary() {
-        return super.getSummary()+": "+this.getText();
-
+    public void setText(String text){
+        super.setText(text);
+        if (textValue!=null)
+            textValue.setText(getText());
     }
+
 }
+
