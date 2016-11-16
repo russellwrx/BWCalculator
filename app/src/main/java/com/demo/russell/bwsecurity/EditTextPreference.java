b833843f081b4dda25fe29aaacf53411da38afa8
russellwrx <russell.gordeyev@gmail.com>
2016-11-16 11:23:01 +1100

    fix theme colors preference

--- app/src/main/java/com/demo/russell/bwsecurity/EditTextPreference.java
+++ app/src/main/java/com/demo/russell/bwsecurity/EditTextPreference.java
@@ -1,9 +1,13 @@
 package com.demo.russell.bwsecurity;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 public class EditTextPreference extends android.preference.EditTextPreference {
@@ -39,6 +43,36 @@ public class EditTextPreference extends android.preference.EditTextPreference {
         super.setText(text);
         if (textValue!=null)
             textValue.setText(getText());
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+        final Resources res = getContext().getResources();
+        final Window window = getDialog().getWindow();
+
+        final int bkColor = res.getColor(android.R.color.black);
+        final int bkColor2 = res.getColor(android.R.color.darker_gray);
+
+        // Title
+        final int titleId = res.getIdentifier("alertTitle", "id", "android");
+        final View title = window.findViewById(titleId);
+        if (title != null) {
+            ((TextView) title).setTextColor(bkColor);
+        }
+
+        // Title divider
+        final int titleDividerId = res.getIdentifier("titleDivider", "id", "android");
+        final View titleDivider = window.findViewById(titleDividerId);
+        if (titleDivider != null) {
+            titleDivider.setBackgroundColor(bkColor2);
+        }
+//
+//        // EditText
+//        final View editText = window.findViewById(android.R.id.edit);
+//        if (editText != null) {
+//            editText.setBackgroundColor(Color.BLACK);
+//        }
     }
 
 }
